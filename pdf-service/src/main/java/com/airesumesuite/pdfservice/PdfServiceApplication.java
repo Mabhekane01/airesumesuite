4afa41073aa869f864f52d7871edc11bcd5505d9
Bheki Ntando <mabhekane01>
2025-08-17 09:07:09 +0200

    09:07 2025/08/17

--- pdf-service/src/main/java/com/airesumesuite/pdfservice/PdfServiceApplication.java
+++ pdf-service/src/main/java/com/airesumesuite/pdfservice/PdfServiceApplication.java
@@ -3,7 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = "com.airesumesuite.pdfservice")
 public class PdfServiceApplication {
 
     public static void main(String[] args) {
